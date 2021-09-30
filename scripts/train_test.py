2bd8a4ecb0601cded230f20b0678d38a6381c418
Debojyoti <debojyoti.das@umu.se>
2021-09-30 15:41:14 +0200

    train-test datasets splitting function

--- scripts/train_test.py
+++ scripts/train_test.py
@@ -13,7 +13,7 @@ def train_test(gex,peaks,train_percent=0.70):
 
     samples_indices = gex.obs.index.tolist()
 
-    training_set_samples = random.sample(samples_indices,int(0.70*float(len(samples_indices))))
+    training_set_samples = random.sample(samples_indices,int(train_percent*float(len(samples_indices))))
     test_set_samples = [s for s in samples_indices if s not in training_set_samples]
 
     train_gex = gex[gex.obs.index.isin(training_set_samples)].copy()
